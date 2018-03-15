e85ed193e34c23b3778788f4134a44c64d23f267
Mohammed Alkhateeb <mohmmad.alkhateeb97.ma@gmail.com>
2018-03-15 09:14:26 +0100

    added js and styled it

--- challenge.js
+++ challenge.js
@@ -2,10 +2,12 @@
 
 function f2() {
     var x = document.getElementById("Date").value;
-    console.log(x);
     if (x > '2000-01-01') {
         alert('you are under age');
-    } else {
+    } else if (x === '') {
+        alert('please put in your age...');
+    }
+     else {
         alert('you are one of us');
     }
 }
